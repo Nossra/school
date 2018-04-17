bd78634de5ab2d51f2ca82934b6291ea4b28b378
Martin Nilsson <mart.nilss@gmail.com>
2018-04-17 16:29:19 +0200

    rest

--- School/src/main/java/se/consys/controllers/TeacherController.java
+++ School/src/main/java/se/consys/controllers/TeacherController.java
@@ -5,25 +5,25 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import se.consys.Entities.Teacher;
 import se.consys.dataaccess.DaoGenericHibernateImpl;
 import se.consys.services.GenericService;
 
 @Path("teachers")
-@Produces("application/json")
-@Consumes("application/json")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class TeacherController {
 	
-	GenericService teacherService;
+	private GenericService teacherService;
 
 	public TeacherController()  {
 		this.teacherService = GenericService.getGenericService(new DaoGenericHibernateImpl<>(Teacher.class));
 	}
 	
 	@GET
-	public Response getAllTeachers() {
+	public Response get() {
 		@SuppressWarnings("unchecked")
 		List<Teacher> allTeachers = teacherService.findAll();
 		return Response.ok(allTeachers).build();
