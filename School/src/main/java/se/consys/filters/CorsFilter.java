0987c2e9927994eacce2ed3fa8c2bc5039d7ce79
Martin Nilsson <mart.nilss@gmail.com>
2018-05-21 23:55:13 +0200

    fixed the corsfilter, now works with front-end

--- School/src/main/java/se/consys/filters/CorsFilter.java
+++ School/src/main/java/se/consys/filters/CorsFilter.java
@@ -1,56 +1,55 @@
-//package se.consys.filters;
-//
-//import java.io.IOException;
-//import javax.servlet.Filter;
-//import javax.servlet.FilterChain;
-//import javax.servlet.FilterConfig;
-//import javax.servlet.ServletException;
-//import javax.servlet.ServletRequest;
-//import javax.servlet.ServletResponse;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpServletResponse;
-//
-///**
-// * Servlet Filter implementation class CorsFilter
-// */
-//@WebFilter("/CorsFilter")
-//public class CorsFilter implements Filter {
-//
-//    /**
-//     * Default constructor. 
-//     */
-//    public CorsFilter() {
-//        // TODO Auto-generated constructor stub
-//    }
-//
-//	/**
-//	 * @see Filter#destroy()
-//	 */
-//	public void destroy() {
-//		// TODO Auto-generated method stub
-//	}
-//
-//	/**
-//	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-//	 */
-//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//		HttpServletResponse res = (HttpServletResponse) response;
-//		String origin = res.getHeader("Access-Control-Allow-Origin"); 
-//		
-//		if (origin == null) {
-//			res.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-//			res.addHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Authorization");
-//			res.addHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
-//		}
-//		
-//		chain.doFilter(request, response);
-//	}
-//
-//	/**
-//	 * @see Filter#init(FilterConfig)
-//	 */
-//	public void init(FilterConfig fConfig) throws ServletException {
-//		// TODO Auto-generated method stub
-//	}
-//
-//}
+package se.consys.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class CorsFilter
+ */
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public CorsFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse res = (HttpServletResponse) response;
+		String origin = res.getHeader("Access-Control-Allow-Origin"); 
+		if (origin == null) {
+			res.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+			res.addHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin");
+			res.addHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
